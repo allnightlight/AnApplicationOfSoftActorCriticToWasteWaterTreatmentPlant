2d53bd7d2ddf9afe5dd0d569046d822eafa5d9e9
Shouta Ukai <shouta.ukai@gmail.com>
2020-11-24 16:41:57 +0900

    Plant002 represent first-order time delay system

---
+++ plants/concrete_plant002.py
@@ -0,0 +1,45 @@
+'''
+Created on 2020/11/23
+
+@author: ukai
+'''
+import numpy as np
+from skeleton.abstract_plant import AbstractPlant
+
+class ConcretePlant002(AbstractPlant):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, threshold = 2., tau = 10.):
+        super(ConcretePlant002, self).__init__()
+        
+        self.sv = None
+        self.x = None
+        self.threshold = threshold
+        self.tau = tau
+
+    def getPv(self):
+        return np.array(self.x).reshape(1,-1).astype(np.float32)
+    
+    def reset(self):
+        self.sv = 1.
+        self.x = 0.
+        
+    def update(self, u):
+        
+        u = u.copy()
+        
+        u[u > self.threshold ] = self.threshold
+        u[u < -self.threshold] = -self.threshold
+        
+        self.x = (1-1/self.tau) * self.x + 1/self.tau * u
+        reward = -(self.sv - self.x).__abs__()
+        
+        return reward
+    
+    def getNmv(self):
+        return 1
+    
+    def getNpv(self):
+        return 1
--- plants/factory_for_test.py
+++ plants/factory_for_test.py
@@ -3,11 +3,14 @@ Created on 2020/11/23
 
 @author: ukai
 '''
+import tensorflow
+
+from concrete.concrete_batch_data_agent import ConcreteBatchDataAgent
 import numpy as np
 from plants.concrete_plant001 import ConcretePlant001
+from plants.concrete_plant002 import ConcretePlant002
 from skeleton.abstract_environment import AbstractEnvironment
-from concrete.concrete_batch_data_agent import ConcreteBatchDataAgent
-import tensorflow
+
 
 class FactoryForTest(object):
     '''
@@ -30,6 +33,23 @@ class FactoryForTest(object):
         return AbstractEnvironment(plant = ConcretePlant001())
     
     def generateBatchDataAgentForPlant001(self):
+        
+        nMv = 1
+        nBatch = 1
+        
+        for _ in range(10):
+            yield ConcreteBatchDataAgent(_Mean = tensorflow.random.normal(shape = (nBatch, nMv))
+                                     , _LogSd = tensorflow.random.normal(shape = (nBatch, nMv)))            
+            
+    def createPlant002(self):
+        
+        return ConcretePlant002()
+    
+    def createEnvironmentPoweredByPlant002(self):
+        
+        return AbstractEnvironment(plant = ConcretePlant002())
+    
+    def generateBatchDataAgentForPlant002(self):
         
         nMv = 1
         nBatch = 1
