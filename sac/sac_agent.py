19385c8b0b420f0d0937dc991fd609a3c77f5431
Shouta Ukai <shouta.ukai@gmail.com>
2020-12-14 07:20:52 +0900

    Bellman equation is not normalized by discount factor

--- sac/sac_agent.py
+++ sac/sac_agent.py
@@ -120,4 +120,4 @@ class SacAgent(object):
         batchDataActionValue = self.valueFunctionApproximator.getActionValue(self.featureExtractor.call(batchDataEnvironment), batchDataAgent)
         batchDataStateValueNext = self.valueFunctionApproximator.getStateValue(self.featureExtractor.call(batchDataEnvironmentNextStep))
         
-        return (batchDataActionValue.getValueWithRedundancy() - ((1-self.discountFactor) * batchDataReward.getValue() + self.discountFactor * batchDataStateValueNext.getValue()))**2
+        return (batchDataActionValue.getValueWithRedundancy() - (batchDataReward.getValue() + self.discountFactor * batchDataStateValueNext.getValue()))**2
