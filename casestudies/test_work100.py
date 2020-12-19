b0897adf12a8ef5b083beea2763d8861cc3de6dc
Shouta Ukai <shouta.ukai@gmail.com>
2020-12-19 14:17:10 +0900

    Work100 track q-function and policy over learning process

---
+++ casestudies/test_work100.py
@@ -0,0 +1,74 @@
+'''
+Created on 2020/12/16
+
+@author: ukai
+'''
+import unittest
+
+from casestudies.work100 import Work100
+from concrete.concrete_build_parameter import ConcreteBuildParameter
+import matplotlib.pylab as plt
+import numpy as np
+import os
+
+
+class Test(unittest.TestCase):
+    
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        self.work = Work100.create()
+        
+        self.buildParameter = ConcreteBuildParameter(nIterationPerEpoch=1)
+        self.nEpoch = 2**2
+        self.nMesh = 2**3
+        self.nInterval = 2**1
+    
+    def test001(self):
+        
+        work = self.work        
+        
+        agent, environment, trainer = work.createAgentEnvironmentAndTrainer(self.buildParameter)
+
+        for _ in work.trainIterator(trainer, self.nEpoch):
+            pass
+        
+        Pv, Mv, Adv = work.getAdvantageOnMesh(agent, nMesh = self.nMesh)
+        
+        for x in (Pv, Mv, Adv):
+            assert x.shape == (self.nMesh, self.nMesh)
+            
+        fig = plt.figure()
+        ax = fig.add_subplot(2,2,(1,3))
+        work.showAdvantageOnMesh(Pv, Mv, Adv, ax, fig)
+        #plt.show()
+        plt.close(fig)
+        
+        Pv, Mv = work.getTrainingData(environment, nLast = None)
+        
+        fig = plt.figure()
+        ax1 = fig.add_subplot(2,2,2)
+        ax2 = fig.add_subplot(2,2,4)
+        work.showTrainingData(Pv, Mv, ax1, ax2, fig)
+        #plt.show()
+        plt.close(fig)        
+        
+    
+    def test002(self):
+        
+        for figPath in self.work.work(self.buildParameter, nEpoch = self.nEpoch, nInterval=self.nInterval, nMesh=self.nMesh, prefix="test", figSize = [20, 8]):
+            os.remove(figPath)
+            
+            
+    def test003(self):
+        
+        Mv = np.random.randn(2**10, 3) * 100        
+        Do = self.work.convertMv2Do(Mv)
+        
+        assert np.all(Do <= self.work.maxDo)
+        assert np.all(Do >= self.work.minDo)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test001']
+    unittest.main()
---
+++ casestudies/work100.py
@@ -0,0 +1,204 @@
+'''
+Created on 2020/12/16
+
+@author: ukai
+'''
+
+import itertools
+import os
+import sys
+
+from concrete.concrete_agent import ConcreteAgent
+from concrete.concrete_agent_factory import ConcreteAgentFactory
+from concrete.concrete_batch_data_agent import ConcreteBatchDataAgent
+from concrete.concrete_batch_data_environment import ConcreteBatchDataEnvironment
+from concrete.concrete_batch_data_value import ConcreteBatchDataValue
+from concrete.concrete_environment import ConcreteEnvironment
+from concrete.concrete_environment_factory import ConcreteEnvironmentFactory
+from concrete.concrete_trainer import ConcreteTrainer
+from concrete.concrete_trainer_factory import ConcreteTrainerFactory
+from framework.util import Utils
+import matplotlib.pylab as plt
+import numpy as np
+
+
+class Work100(object):
+    '''
+    classdocs
+    '''
+
+    @classmethod
+    def create(cls, showLog = False):
+        return Work100(agentFactory = ConcreteAgentFactory()
+            , environmentFactory = ConcreteEnvironmentFactory()
+            , trainerFactory = ConcreteTrainerFactory()
+            , showLog = showLog)
+
+    def __init__(self, agentFactory, environmentFactory, trainerFactory, thresholdDo = 1.5, Sv_Nh4 = 3.0, minDo = 0.0, maxDo = 3.0, figFolder = "./fig", showLog = False, limDo = [0,3], limNh4 = [0, 6]):
+        '''
+        Constructor
+        '''
+        
+        assert isinstance(agentFactory, ConcreteAgentFactory)
+        assert isinstance(environmentFactory, ConcreteEnvironmentFactory)
+        assert isinstance(trainerFactory, ConcreteTrainerFactory)
+        
+        self.agentFactory = agentFactory
+        self.environmentFactory = environmentFactory
+        self.trainerFactory = trainerFactory        
+        self.thresholdDo = thresholdDo
+        self.Sv_Nh4 = Sv_Nh4
+        self.minDo = minDo
+        self.maxDo = maxDo
+        self.figFolder = figFolder
+        self.showLog = showLog
+        self.limDo = limDo
+        self.limNh4 = limNh4
+        
+    def createAgentEnvironmentAndTrainer(self, buildParameter):
+        
+        environment = self.environmentFactory.create(buildParameter)
+        agent = self.agentFactory.create(buildParameter, environment)
+        trainer = self.trainerFactory.create(buildParameter, agent, environment)
+
+        return agent, environment, trainer
+    
+    def trainIterator(self, trainer, nEpoch):
+
+        assert isinstance(trainer, ConcreteTrainer)
+        
+        trainer.reset()
+        trainer.trainPreprocess()
+        for k1 in range(nEpoch):
+            trainer.train()
+            yield k1
+
+    def convertPv2Nh4(self, Pv):
+        return Pv + self.Sv_Nh4
+    
+    def convertMv2Do(self, Mv):
+        MvChopped = Mv.copy()
+        MvChopped[Mv > self.thresholdDo] = self.thresholdDo
+        MvChopped[Mv < -self.thresholdDo] = -self.thresholdDo
+        return (MvChopped + self.thresholdDo)/(2*self.thresholdDo) * (self.maxDo-self.minDo) + self.minDo
+                                                
+    def getAdvantageOnMesh(self, agent, nMesh):
+        '''
+        Pv: Pv
+        Mv: Mv
+        Adv: Advantage        
+        '''
+        
+        assert isinstance(agent, ConcreteAgent)
+        
+        Pv, Mv = np.meshgrid(np.linspace(self.limNh4[0]-self.Sv_Nh4, self.limNh4[1]-self.Sv_Nh4,nMesh), np.linspace(-self.thresholdDo, self.thresholdDo,nMesh))
+        Pv = Pv.astype(np.float32)
+        Mv = Mv.astype(np.float32)
+        Adv = np.zeros((nMesh, nMesh))
+        
+        for k1, k2 in itertools.product(range(nMesh), range(nMesh)):
+        
+            e = Pv[k1,k2].reshape(1,1) # (1,1)
+            u = Mv[k1,k2].reshape(1,1) # (1,1)
+            batchDataEnvironment = ConcreteBatchDataEnvironment(bufferPv = [e,], bufferMv = [])        
+            batchDataAgent = ConcreteBatchDataAgent(_Mean = None, _LogSd = None, sampledAction = u)
+            
+            batchDataValue = agent.valueFunctionApproximator.getActionValue(batchDataFeature = agent.featureExtractor.call(batchDataEnvironment)
+                                                           , batchDataAgent = batchDataAgent)
+            
+            batchDataValue2 = agent.valueFunctionApproximator.getStateValue(batchDataFeature = agent.featureExtractor.call(batchDataEnvironment))
+            
+            assert isinstance(batchDataValue, ConcreteBatchDataValue)
+            
+            v = batchDataValue.getValue() - batchDataValue2.getValue() # (1,1)
+        
+            Adv[k1,k2] = v
+            
+        return Pv, Mv, Adv
+    
+    def showAdvantageOnMesh(self, Pv, Mv, Adv, ax, fig):
+                
+        assert isinstance(fig, plt.Figure)
+        assert isinstance(ax, plt.Axes)
+        
+        c = ax.pcolor(self.convertPv2Nh4(Pv), self.convertMv2Do(Mv), Adv)
+        ax.set_xlabel("NH4")
+        ax.set_ylabel("DO")    
+        fig.colorbar(c, ax=ax)
+                
+    def getTrainingData(self, environment, nLast = None):
+        
+        assert isinstance(environment, ConcreteEnvironment)
+        
+        Pv = []
+        Mv = []
+        for e, u in zip(environment.bufferPv, environment.bufferMv):
+            
+            Pv.append(e)
+            Mv.append(u)
+            
+        Pv = np.concatenate(Pv, axis=0) # (..., nPv)
+        Mv = np.concatenate(Mv, axis=0) # (..., nMv)
+        
+        if nLast is not None:
+            Pv = Pv[-nLast:]
+            Mv = Mv[-nLast:]
+                    
+        return Pv, Mv
+        
+    def showTrainingData(self, Pv, Mv, ax1, ax2, fig):
+        
+        assert isinstance(fig, plt.Figure)
+        assert isinstance(ax1, plt.Axes)
+        assert isinstance(ax2, plt.Axes)
+        
+        ax1.plot(self.convertPv2Nh4(Pv))
+        ax1.set_ylabel("NH4")
+        ax1.set_ylim(self.limNh4)
+        ax2.plot(self.convertMv2Do(Mv))
+        ax2.set_ylabel("DO")
+        ax2.set_ylim(self.limDo)
+        
+    def watchLearningProcess(self, buildParameter, nEpoch, nInterval, fig, nMesh):
+        
+        assert isinstance(fig, plt.Figure)
+                
+        agent, environment, trainer = self.createAgentEnvironmentAndTrainer(buildParameter)
+        
+        for k1 in self.trainIterator(trainer, nEpoch):
+            
+            if self.showLog:
+                sys.stdout.write("\r>> %4d/%4d" % (k1+1, nEpoch))            
+            
+            if (k1 + 1) % nInterval == 0:
+                
+                fig.clf()                
+                ax0 = fig.add_subplot(2,2,(1,3))
+                self.showAdvantageOnMesh(*self.getAdvantageOnMesh(agent, nMesh), ax0, fig) 
+                ax1 = fig.add_subplot(2,2,2)
+                ax2 = fig.add_subplot(2,2,4)
+                self.showTrainingData(*self.getTrainingData(environment, nLast = nInterval), ax1, ax2, fig) 
+                fig.tight_layout()
+                
+                yield
+                
+    def work(self, buildParameter
+            , nEpoch = 96 * 28
+            , nInterval = 96
+            , nMesh = 2**4
+            , prefix = Utils.generateRandomString(16)
+            , figSize = [20, 8]):
+        
+        if not os.path.exists(self.figFolder):
+            os.mkdir(self.figFolder)
+        
+        fig = plt.figure(figsize=figSize)
+        
+        for k1, _ in enumerate(self.watchLearningProcess(buildParameter, nEpoch, nInterval, fig, nMesh)):
+            day = k1 + 1
+            
+            fname = "%s_day=%2d.png" % (prefix, day)
+            figPath = os.path.join(self.figFolder, fname)        
+            fig.savefig(figPath)
+            
+            yield figPath
