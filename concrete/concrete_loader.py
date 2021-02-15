dcd318ecb264f2157e32417a94a5aabcac29c2ea
Shouta Ukai <shouta.ukai@gmail.com>
2021-02-15 16:08:57 +0900

    Moved the feature of concrete/loader to list agent keys to framework/loader

--- concrete/concrete_loader.py
+++ concrete/concrete_loader.py
@@ -8,7 +8,6 @@ from concrete.concrete_environment_factory import ConcreteEnvironmentFactory
 from concrete.concrete_trainer_factory import ConcreteTrainerFactory
 from framework.loader import Loader
 from concrete.concrete_build_parameter_factory import ConcreteBuildParameterFactory
-from framework.store_field import StoreField
 
 
 class ConcreteLoader(Loader):
@@ -25,13 +24,3 @@ class ConcreteLoader(Loader):
                          , store = store)
         
         
-    def getPairsOfAgentKeyAndEpoch(self, buildParameterLabel, epoch, buildParameterKey, agentKey):
-        
-        self.store.update_db()
-        
-        lst = []
-        for storeField in self.store.restore(buildParameterLabel, epoch, buildParameterKey, agentKey):
-            assert isinstance(storeField, StoreField)            
-            lst.append((storeField.agentKey, storeField.epoch))
-        
-        return lst
